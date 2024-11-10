bb3de95b178a9f90d751945836b90b1b6b202d63
sourabhgorule <gorulesourabh@gmail.com>
2024-11-10 19:18:32 +0530

    sourabh - first commit

---
+++ src/main/java/com/curiocity/CuriocityApplication.java
@@ -0,0 +1,13 @@
+package com.curiocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuriocityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuriocityApplication.class, args);
+	}
+
+}
